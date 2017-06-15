b1c45a8a0d92856982113309dfcda3d855f0d68e
Brian Hume <brian.hume@globant.com>
2017-06-15 12:25:07 -0300

    Removed boiler components

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 
-import { Header } from "./components/Header"
-import { Home } from "./components/Home"
-
 import Request from 'superagent';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    
-    var url = `https://s3.amazonaws.com/technical-challenge/Contacts_v2.json`;
-    Request.get(url).then((response) => {
-      console.log(response.body);
-    });
-  }
+  // constructor(){
+  //   super();
+
+  //   // var url = `https://s3.amazonaws.com/technical-challenge/Contacts_v2.json`;
+  //   // Request.get(url).then((response) => {
+  //   //   console.log(response.body);
+  //   // });
+  // }
 
   render() {
 
